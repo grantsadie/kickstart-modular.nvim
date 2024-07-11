049f58fb7f8830b8801ad8deb2b9fed5dfa3c707
Grant Sadie <grantdsadie@gmail.com>
2024-07-11 20:21:04 +1000

    TMux navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -50,3 +50,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.api.nvim_set_keymap('n', '<F4>', ':ClangdSwitchSourceHeader<CR>', { noremap = true, silent = true })
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'cpp',
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
